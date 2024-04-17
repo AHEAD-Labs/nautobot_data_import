bd0c99cc6d127c3962544d79db9f3554e49dd317
BrianNelson <brian.nelson@ahead.com>
2024-04-17 08:59:35 -0600

    removed Sample

--- jobs/__init__.py
+++ jobs/__init__.py
@@ -1,4 +1,3 @@
 from nautobot.apps.jobs import Job, register_jobs
-from .sample import Sample
 from .sevone_import import Sevone_Onboarding
-register_jobs(Sample, Sevone_Onboarding)
+register_jobs(Sevone_Onboarding)
--- jobs/sample.py
+++
@@ -1,7 +0,0 @@
-from nautobot.apps.jobs import Job, register_jobs
-class Sample(Job):
-    def run(self, data, commit):
-        self.data = data
-        self.commit = commit
-        self.log_success(message='Job Ran')
-register_jobs(Sample)
